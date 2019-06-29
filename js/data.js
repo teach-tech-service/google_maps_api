3549a84a887ab24c0cdd68915a27d8a115ae7ab8
codemasternode <marcinwarzybok@outlook.com>
2019-06-29 21:48:10 +0200

    commited

---
+++ js/data.js
@@ -0,0 +1,47 @@
+let places = [
+    {
+        latitude: 50.056342,
+        longitude: 19.940276,
+        street: "Św Gertrudy",
+        number: 30,
+        zip: "31-046",
+        city: "Kraków",
+        package_type: "package"
+    },
+    {
+        latitude: 50.063775,
+        longitude: 19.939625,
+        street: "Św Marka",
+        number: 17,
+        zip: "31-501",
+        city: "Kraków",
+        package_type: "priority_letter"
+    },
+    {
+        latitude: 50.064332,
+        longitude: 19.957168,
+        street: "Mikołaja Kopernika",
+        number: 17,
+        zip: "31-501",
+        city: "Kraków",
+        package_type: "cash"
+    },
+    {
+        latitude: 50.064332,
+        longitude: 19.957168,
+        street: "Mikołaja Kopernika",
+        number: 17,
+        zip: "31-501",
+        city: "Kraków",
+        package_type: "cash"
+    },
+    {
+        latitude: 50.064332,
+        longitude: 19.957168,
+        street: "Mikołaja Kopernika",
+        number: 17,
+        zip: "31-501",
+        city: "Kraków",
+        package_type: "cash"
+    }
+];
